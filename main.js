74c6361609cfdc69c1adf8bbcf59af022a49844a
Mohammed Asker <zetoanime@gmail.com>
2019-06-25 10:31:30 +0300

    created all files of the project

---
+++ main.js
@@ -0,0 +1,43 @@
+// INTRO STAGE
+//start conversation
+	function btn() {
+		document.getElementById('text').innerHTML = 'Welcome to the JS Dojo!';
+		document.getElementById('showBtn').style.display = 'block';
+	}
+// Master JavaScript introducing itself
+	function btn2() {
+		document.getElementById('text2').innerHTML = "My name is JavaScript and I'm here to teach you every skills I know to help you become the strongest Coding Warrior in the world!";
+		document.getElementById('showBtn2').style.display = 'block';
+	}
+// Ready for training?
+	function btn3() {
+		document.getElementById('text3').innerHTML = "The training will be a living hell! Are you ready?";
+		document.getElementById('showBtn3').style.display = 'block';
+		document.getElementById('showBtn3').style.display = 'block';
+	}
+// Select Yes
+	function btn4() {
+		document.getElementById('text4').innerHTML = "Good! Then, let's begin by going to the first stage";
+		document.getElementById('showBtn4').style.display = 'block';
+	}
+// Select No
+	function btn5() {
+		document.getElementById('text5').innerHTML = "I see. When you're ready for the training, let me know."; 
+	    document.getElementById('showBtn5').style.display = 'block';
+	}
+	function tryBtn(){
+		window.location.reload()
+	}
+	
+// Disable the button after selecting the answer
+/*if user select yes button
+  disable no button
+  else
+  disable yes button*/
+
+// End of Intro Stage
+
+// STAGE 1
+// STAGE 2
+// STAGE 3
+// FINAL STAGE
