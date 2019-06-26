eaa4e528e31e1bbb937b03870eac22b66bbc14e5
Mohammed Asker <zetoanime@gmail.com>
2019-06-26 11:59:07 +0300

    got intro stage buttons working and added CSS styles

--- main.js
+++ main.js
@@ -9,35 +9,33 @@
 		document.getElementById('text2').innerHTML = "My name is JavaScript and I'm here to teach you every skills I know to help you become the strongest Coding Warrior in the world!";
 		document.getElementById('showBtn2').style.display = 'block';
 	}
-// Ready for training?
+// Master asks if player is ready
 	function btn3() {
 		document.getElementById('text3').innerHTML = "The training will be a living hell! Are you ready?";
 		document.getElementById('showBtn3').style.display = 'block';
 		document.getElementById('showBtn3').style.display = 'block';
 	}
-// Select Yes
-	function btn4() {
-		document.getElementById('text4').innerHTML = "Good! Then, let's begin by going to the first stage";
+// Player selects Yes
+	if (document.getElementById('btn4').onclick = function() {
+		document.getElementById('btn5').disabled = true;
+		document.getElementById('text4').innerHTML = "Good! Then, let's begin by going to the first stage.";
 		document.getElementById('showBtn4').style.display = 'block';
-	}
-// Select No
-	function btn5() {
+	})
+// Player selects No
+	if (document.getElementById('btn5').onclick = function() {
+		document.getElementById('btn4').disabled = true;
 		document.getElementById('text5').innerHTML = "I see. When you're ready for the training, let me know."; 
 	    document.getElementById('showBtn5').style.display = 'block';
-	}
+	})
+
+// Spawn Retry button when selected No
 	function tryBtn(){
 		window.location.reload()
 	}
-	
-// Disable the button after selecting the answer
-/*if user select yes button
-  disable no button
-  else
-  disable yes button*/
-
 // End of Intro Stage
 
 // STAGE 1
+
 // STAGE 2
 // STAGE 3
 // FINAL STAGE
